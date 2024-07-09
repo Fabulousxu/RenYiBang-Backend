96abb5d8cd5cd8cd802dd5bf90c114375e1b5b38
Eric-mizhou <jy31zhy@163.com>
2024-07-09 17:20:18 +0800

    ServiceImpl and Dao not finished

---
+++ ServiceApi/src/main/java/com/renyibang/serviceapi/entity/ServiceAccess.java
@@ -0,0 +1,35 @@
+package com.renyibang.serviceapi.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "service_access")
+@Getter
+@Setter
+@NoArgsConstructor
+public class ServiceAccess {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long serviceAccessId; // 服务接取候选id
+
+  @ManyToOne
+  @JoinColumn(name = "service_id")
+  private Service service; // 服务
+
+  @Column(name = "accessor_id")
+  private long accessor_id; // 服务接取者
+
+  @CreationTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+  private LocalDateTime createdAt; // 服务接取时间
+
+  @Column(name = "valid")
+  private boolean valid = true; //接取候选是否有效
+}
