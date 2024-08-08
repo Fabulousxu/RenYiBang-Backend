326185ebfdd8534b97eb713e143b694bf10e071c
iloveics <412268070@qq.com>
2024-08-08 22:07:55 +0800

    新增Gateway 的 ccontroller

--- Gateway/src/main/java/com/renyibang/gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/renyibang/gateway/GatewayApplication.java
@@ -1,20 +1,13 @@
 package com.renyibang.gateway;
 
-import com.renyibang.global.util.Response;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
-	@GetMapping("/api/gateway/health")
-	public Response health() {
-		return Response.success();
-	}
-
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.configure().load();
 		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
