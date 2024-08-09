b8a640629173b65dea5aa0fcc7a21d391d13603c
wkzMagician <wkzmagician@gmail.com>
2024-08-09 23:18:11 +0800

    Update AuthServiceImpl.java

--- UserApi/src/main/java/com/renyibang/userapi/service/serviceImpl/AuthServiceImpl.java
+++ UserApi/src/main/java/com/renyibang/userapi/service/serviceImpl/AuthServiceImpl.java
@@ -33,6 +33,7 @@ public class AuthServiceImpl implements AuthService {
     user.setNickname(register.getNickname());
     user.setPhone(register.getPhone());
     user.setEmail(register.getEmail());
+    user.setIntro("");
     userRepository.save(user);
     Auth auth = new Auth();
     auth.setUserId(user.getUserId());
